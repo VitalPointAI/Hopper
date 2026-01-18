a489f8dd9b75dccf36f7db103ec8493149587d51
Aaron Luhning <a.luhning@vitalpoint.ai>
2026-01-18 12:14:51 -0500

    feat(07-01): create config module index
    - Re-exports all types: PlanningDepth, ExecutionMode, HopperConfig
    - Re-exports ConfigManager class
    - Re-exports DEFAULT_CONFIG and createDefaultConfig
    - Follows established module pattern

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export { ConfigManager } from './configManager';
+export type {
+  PlanningDepth,
+  ExecutionMode,
+  HopperConfig
+} from './types';
+export { DEFAULT_CONFIG, createDefaultConfig } from './types';
